35e219932fc4de77bc9e16c3a903ca16ad829119
Mateusz Otrebski <m.otrebski@cez.gov.pl>
2026-04-07 21:44:25 +0200

    app settings

--- Passenger.Api/Controllers/UsersController.cs
+++ Passenger.Api/Controllers/UsersController.cs
@@ -1,8 +1,8 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Passenger.Infrastructure.Commands;
 using Passenger.Infrastructure.Commands.Users;
-using Passenger.Infrastructure.DTO;
 using Passenger.Infrastructure.Services;
 
 namespace Passenger.Api.Controllers
